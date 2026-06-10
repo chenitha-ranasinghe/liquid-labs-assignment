415c42fad08c3e5c04dfd37595eb32bda943da5e
Chenitha Ranasinghe <chenitha.20231701@iit.ac.lk>
2026-06-10 15:08:00 +0530

    Add Alpha Vantage API service to fetch monthly data

---
+++ services/alpha_vantage.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+# load the API key from .env file
+load_dotenv()
+
+ALPHA_VANTAGE_URL = "https://www.alphavantage.co/query"
+
+def fetch_monthly_data(symbol: str):
+    # get API key from .env file
+    api_key = os.getenv("ALPHA_VANTAGE_KEY")
+
+    # if API key missing raise Value error
+    if not api_key:
+        raise ValueError("ALPHAVANTAGE_API_KEY not in .env file")
+
+    # parameters
+    params = {
+        "function" : "TIME_SERIES_MONTHLY", # for get monthly data
+        "symbol" : symbol,  # for add symbol
+        "apikey" : api_key
+    }
+
+    # Call API
+    response = requests.get(ALPHA_VANTAGE_URL, params=params)
+    if response.status_code != 200:
+        raise ConnectionError(f"Failed to reach Alpha Vantage: {response.status_code}")
+
+    # The responses are convert to JSON dict
+    data = response.json()
+
+    #if simbol is not valid
+    if "Monthly Time Series" not in data:
+        raise ValueError(f"No data found from symbol: {symbol}")
+
+    return data["Monthly Time Series"]
+
+
+
+
